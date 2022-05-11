cac58300aa10b72b4075ec94bc62b9afc4d6a89c
jkoenig <julian.koenig@js-soft.com>
2022-05-11 09:30:26 +0200

    chore: improve

--- src/eventBus/EventBus.ts
+++ src/eventBus/EventBus.ts
@@ -1,19 +1,23 @@
 import { Event } from "../events/Event";
 
 export type EventHandler<TEvent> = (event: TEvent) => void;
-export type SubscriptionTarget<TEvent> = string | (new () => TEvent);
+export type EventConstructor<TEvent> = new (...args: any[]) => TEvent;
+export type SubscriptionTarget<TEvent> = string | EventConstructor<TEvent>;
 
 export abstract class EventBus {
     public abstract subscribe<TEvent = any>(namespace: string, handler: EventHandler<TEvent>): number;
-    public abstract subscribe<TEvent = any>(eventConstructor: new () => TEvent, handler: EventHandler<TEvent>): number;
+    public abstract subscribe<TEvent>(
+        eventConstructor: EventConstructor<TEvent>,
+        handler: EventHandler<TEvent>
+    ): number;
     public abstract subscribe<TEvent = any>(
         subscriptionTarget: SubscriptionTarget<TEvent>,
         handler: EventHandler<TEvent>
     ): number;
 
     public abstract subscribeOnce<TEvent = any>(namespace: string, handler: EventHandler<TEvent>): number;
-    public abstract subscribeOnce<TEvent = any>(
-        eventConstructor: new () => TEvent,
+    public abstract subscribeOnce<TEvent>(
+        eventConstructor: EventConstructor<TEvent>,
         handler: EventHandler<TEvent>
     ): number;
     public abstract subscribeOnce<TEvent = any>(
