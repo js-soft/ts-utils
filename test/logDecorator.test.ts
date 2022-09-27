673cad92d97c8f4c1b9d6f4afa1e3d04362c0ca8
jkoenig <julian.koenig@js-soft.com>
2022-09-27 13:13:58 +0200

    chore: update tests

--- test/logDecorator.test.ts
+++ test/logDecorator.test.ts
@@ -1,13 +1,25 @@
 import { log } from "../src";
 
+class ErrorWithoutStack extends Error {
+    public constructor(message: string) {
+        super(message);
+        this.stack = undefined;
+    }
+}
+
 class MockLogger {
     public traceLog: any[][] = [];
     public trace(...args: any[]): void {
         this.traceLog.push(args);
     }
 
+    private errorToString(error: Error): string {
+        return `${error.name}: ${error.message}${error.stack ? `\n${error.stack}` : ""}`;
+    }
+
     public errorLog: any[][] = [];
     public error(...args: any[]): void {
+        args = args.map((arg) => (arg instanceof Error ? this.errorToString(arg) : arg));
         this.errorLog.push(args);
     }
 
@@ -45,7 +57,7 @@ class DecoratedClass {
     }
 
     @log({ logParams: true })
-    public logParams<T>(value: T): T {
+    public logParams<T>(...value: T[]): T[] {
         return value;
     }
 
@@ -68,6 +80,11 @@ class DecoratedClass {
     public throwError(): void {
         throw new Error("anErrorMessage");
     }
+
+    @log()
+    public callsThrowError(): void {
+        this.throwError();
+    }
 }
 
 describe("@log", () => {
@@ -75,73 +92,73 @@ describe("@log", () => {
     const decoratedClass = new DecoratedClass(logger);
     beforeEach(() => logger.reset());
 
-    test("annotated method does not alter return value", () => {
-        expect(decoratedClass.doNotLogValues("test")).toBe("test");
-        expect(decoratedClass.doNotLogValues({ aKey: "aValue" })).toStrictEqual({ aKey: "aValue" });
-        expect(decoratedClass.doNotLogValues(undefined)).toBeUndefined();
-    });
+    test.each(["test", { aKey: "aValue" }, undefined, null])(
+        "returns original return value for '%p'",
+        (value: unknown) => {
+            expect(decoratedClass.doNotLogValues(value)).toBe(value);
+        }
+    );
 
-    test("should trace log method call and return", () => {
+    test("logs method name on entry and exit", () => {
         decoratedClass.doNotLogValues("test");
 
         expect(decoratedClass.log.traceLog).toStrictEqual([
             ["Calling doNotLogValues"],
             ["Returning from doNotLogValues"]
         ]);
-        expect(decoratedClass.log.errorLog).toHaveLength(0);
     });
 
-    test("should trace log method call and return with params", () => {
-        decoratedClass.logParams("test");
+    test.each(["test", { aKey: "aValue" }, ["aString", "anotherString"], [{ aKey: "aValue" }, { aKey: "aValue" }]])(
+        "when logParams=true, logs parameters ('%p') on entry",
+        (...value: any[]) => {
+            decoratedClass.logParams(...value);
 
-        expect(decoratedClass.log.traceLog).toStrictEqual([
-            ['Calling logParams("test")'],
-            ["Returning from logParams"]
-        ]);
-        expect(decoratedClass.log.errorLog).toHaveLength(0);
-    });
+            expect(decoratedClass.log.traceLog).toStrictEqual([
+                [`Calling logParams(${value.map((v) => JSON.stringify(v)).join(", ")})`],
+                ["Returning from logParams"]
+            ]);
+        }
+    );
 
-    test("should trace log method call and return with return value", () => {
-        decoratedClass.logReturnValue("test");
+    test.each(["test", { aKey: "aValue" }])(
+        "when logReturnValue=true, logs return value ('%p') on exit",
+        (value: any) => {
+            decoratedClass.logReturnValue(value);
 
-        expect(decoratedClass.log.traceLog).toStrictEqual([
-            ["Calling logReturnValue"],
-            ['Returning from logReturnValue with: "test"']
-        ]);
-        expect(decoratedClass.log.errorLog).toHaveLength(0);
-    });
+            expect(decoratedClass.log.traceLog).toStrictEqual([
+                ["Calling logReturnValue"],
+                [`Returning from logReturnValue with: ${JSON.stringify(value)}`]
+            ]);
+        }
+    );
 
-    test("should trace log method call and return with params and return value", () => {
-        decoratedClass.logParamsAndReturnValue("test");
+    test.each([
+        { error: "test", expectedMessage: "test" },
+        { error: new ErrorWithoutStack("anErrorMessage"), expectedMessage: "Error: anErrorMessage" }
+    ])(
+        "logs errors that are thrown for the error: '$error'",
+        (params: { error: string | Error; expectedMessage: string }) => {
+            expect(() => decoratedClass.logError(params.error)).toThrow(params.error);
 
-        expect(decoratedClass.log.traceLog).toStrictEqual([
-            ['Calling logParamsAndReturnValue("test")'],
-            ['Returning from logParamsAndReturnValue with: "test"']
-        ]);
-        expect(decoratedClass.log.errorLog).toHaveLength(0);
-    });
+            expect(decoratedClass.log.errorLog).toStrictEqual([["Error in logError:", params.expectedMessage]]);
+        }
+    );
 
-    test("should log thrown error (string)", () => {
-        expect(() => decoratedClass.logError("test")).toThrow("test");
-
-        expect(decoratedClass.log.traceLog).toStrictEqual([["Calling logError"]]);
-        expect(decoratedClass.log.errorLog).toStrictEqual([["Error in logError: test"]]);
-    });
-
-    test("should log thrown error (error object)", () => {
-        const error = new Error("anErrorMessage");
-        delete error.stack;
-
-        expect(() => decoratedClass.logError(error)).toThrow(error);
-
-        expect(decoratedClass.log.traceLog).toStrictEqual([["Calling logError"]]);
-        expect(decoratedClass.log.errorLog).toStrictEqual([["Error in logError: Error: anErrorMessage"]]);
-    });
-
-    test("should get rid of the logDecorator in the call stack", () => {
+    test("preserves the original stacktrace in case of a simple call (the log decorator call is not included)", () => {
         expect(() => decoratedClass.throwError()).toThrow(new Error("anErrorMessage"));
 
-        expect(decoratedClass.log.traceLog).toStrictEqual([["Calling throwError"]]);
-        expect(decoratedClass.log.errorLog).toStrictEqual([["Error in throwError: Error: anErrorMessage"]]);
+        expect(decoratedClass.log.errorLog).toHaveLength(1);
+
+        const concat = decoratedClass.log.errorLog[0].join(" ");
+        expect(concat).not.toMatch(/logDecorator.ts/);
+    });
+
+    test("preserves the original stacktrace in case of nested calls", () => {
+        expect(() => decoratedClass.callsThrowError()).toThrow(new Error("anErrorMessage"));
+
+        expect(decoratedClass.log.errorLog).toHaveLength(2);
+
+        const concat2 = decoratedClass.log.errorLog[1].join(" ");
+        expect(concat2).not.toMatch(/logDecorator.ts/);
     });
 });
